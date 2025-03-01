08e81b92ef8f79fbadeea0010121150064a314e5
aniruddha <aniruddha27@gmail.com>
2025-03-01 12:34:53 +0530

    Added leaky bucket algorithm.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export interface RateLimiterOptions {
     windowMs: number;
     maxRequests: number;
-    algo?: "sliding" | "fixed" | "token"; // Default: "sliding"
+    algo?: "sliding" | "fixed" | "token" | "leaky"; // Default: "sliding"
     tokensPerInterval?: number; // Required for token bucket
     leakRate?: number; // Only for Leaky Bucket
     errorMessage?: string; // Custom error message
